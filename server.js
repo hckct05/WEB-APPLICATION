6725837d7c332f598a5e702d28e06ae04a24e309
Chi Khang <hckct12345@gmail.com>
2023-02-18 13:17:38 +0700

    Cai dat xu ly loi

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const { post } = require("./app");
 const app = require("./app");
 const config = require("./app/config");
 
+// start server
 const PORT = config.app.port;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
